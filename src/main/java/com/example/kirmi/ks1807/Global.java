6b15d969a7543960ad7a36c28d4666cc3d537b3f
B4LLL <c.millington@westernsydney.edu.au>
2018-11-27 15:26:47 +1100

    Added boolean check for isLogged = false

--- src/main/java/com/example/kirmi/ks1807/Global.java
+++ src/main/java/com/example/kirmi/ks1807/Global.java
@@ -6,14 +6,16 @@ public class Global
     public static String UserID = "";
 
     //Used to pass the current user password to the API.
-    public static String UserPassword = "";
+    static String UserPassword = "";
 
     //Used to pass the Mood ID.
-    public static String MoodID = "";
+    static String MoodID = "";
 
     /*Used if the user backtracks on the connect page (determines if they go to the first or
     second registration page.*/
-    public static String UserExtraMoodQuestions = "";
+    static String UserExtraMoodQuestions = "";
+
+    static Boolean isLogged = false;
 
     //add 2 variables here to declare ->
         //  when the user is signed in
