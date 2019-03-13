87cbc28ba20189a1c7655b388588a184f82e6d66
Christopher Millington <30037043@westernsydney.edu.au>
2019-03-13 11:13:47 +1100

    calendar now finds the dates that exist for month x etc

--- src/main/java/com/example/kirmi/ks1807/DiaryCalendarFragment.java
+++ src/main/java/com/example/kirmi/ks1807/DiaryCalendarFragment.java
@@ -8,30 +8,42 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import sun.bob.mcalendarview.MCalendarView;
 import sun.bob.mcalendarview.listeners.OnMonthChangeListener;
-
-import static android.support.constraint.Constraints.TAG;
+import sun.bob.mcalendarview.vo.DateData;
 
 public class DiaryCalendarFragment extends Fragment {
-    ArrayList<ReturnedDiaryEntry> returnedDiaryList = new ArrayList<>();
     RestInterface.Ks1807Client client;
     Retrofit retrofit = RestInterface.getClient();
+    MCalendarView mCalendarView;
+    Calendar cal;
+    Date dateToday;
+    DateData dateData;
+    int currentYear, currentMonth, currentDay;
+    String diaryData;
+    Boolean calendarPoll = false;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
+        dateToday = new Date();
+        cal = Calendar.getInstance();
+        cal.setTime(dateToday);
+        dateData = new DateData(currentYear = cal.get(Calendar.YEAR),currentMonth = cal.get(Calendar.MONTH) + 1,currentDay = cal.get(Calendar.DAY_OF_MONTH));
         getCalendar(Global.UserID);
+
         super.onCreate(savedInstanceState);
     }
 
     @Override
     public void onResume() {
-        getCalendar(Global.UserID);
         super.onResume();
     }
 
@@ -39,17 +51,23 @@ public class DiaryCalendarFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_diary_calendarfrag,null);
-        MCalendarView mCalendarView = view.findViewById(R.id.calendar);
+        mCalendarView = view.findViewById(R.id.calendar);
+        mCalendarView.travelTo(dateData);
         mCalendarView.setOnMonthChangeListener(new OnMonthChangeListener() {
             @Override
             public void onMonthChange(int year, int month) {
                 Log.d("onMonthChange","\nyear\t" + year + "\nmonth\t" + month);
+                currentYear = year;
+                currentMonth = month;
+                if(calendarPoll)
+                    markCalendar(processDiaryEntry(diaryData));
             }
         });
 
-        //mCalendarView.markDate(year,month,day); use this after getting all the entries for the month
         return view;
     }
+
+    //mCalendarView.markDate(year,month,day); use this after getting all the entries for the month
 
     public void getCalendar(String UserID){
         client = retrofit.create(RestInterface.Ks1807Client.class);
@@ -57,27 +75,34 @@ public class DiaryCalendarFragment extends Fragment {
         response.enqueue(new Callback<String>() {
             @Override
             public void onResponse(Call<String> call, Response<String> response) {
-                Log.d("getCalendar Called","\t" + response);
-                String returnAllEntries = response.body();
-                String[] returnDiaryText = returnAllEntries.split("\f");
-                ArrayList<String> row;
-                for(int i = 0; i < returnDiaryText.length; i++){
-                    returnedDiaryList.add(new ReturnedDiaryEntry(returnDiaryText[i]));
-                }
-                for (ReturnedDiaryEntry x : returnedDiaryList) {
-                    Log.d(TAG, " " + x.getDate());
-                    Log.d(TAG, " " + x.getEntryOne());
-                    Log.d(TAG, " " + x.getEntryTwo());
-                    Log.d(TAG, " " + x.getEntryFour());
-                }
+                if(!response.body().equals("-1")) {
+                    calendarPoll = true;
+                    diaryData = response.body();
+                    markCalendar(processDiaryEntry(diaryData));
+                }else
+                    Toast.makeText(getContext(),"No diary entries found", Toast.LENGTH_SHORT).show();
             }
-
             @Override
             public void onFailure(Call<String> call, Throwable t) {
                 Log.d("getCalendar Called","\terror\t" + t.toString());
             }
         });
     }
-    //retreive all the dates.
 
+    ArrayList<ReturnedDiaryEntry> processDiaryEntry(String returnAllEntries){
+        String[] returnDiaryText = returnAllEntries.split("\f");
+        ArrayList<ReturnedDiaryEntry> returnedDiaryList = new ArrayList<>();
+        for(int i = 0; i < returnDiaryText.length; i++) {
+            returnedDiaryList.add(new ReturnedDiaryEntry(returnDiaryText[i]));
+        }
+        return returnedDiaryList;
+    }
+
+    void markCalendar(ArrayList<ReturnedDiaryEntry> returnedDiaryList){
+        for (ReturnedDiaryEntry x : returnedDiaryList) {
+            if(x.getMonth() == currentMonth){
+                mCalendarView.markDate(currentYear,x.getMonth(),x.getDay());
+            }
+        }
+    }
 }
--- src/main/java/com/example/kirmi/ks1807/ReturnedDiaryEntry.java
+++ src/main/java/com/example/kirmi/ks1807/ReturnedDiaryEntry.java
@@ -1,47 +1,39 @@
 package com.example.kirmi.ks1807;
 
-public class ReturnedDiaryEntry {
-    private String date, entryOne, entryTwo, entryThree, entryFour;
-    String[] splitRow;
-
-    public ReturnedDiaryEntry(String returnDiaryText){
-        splitRow = returnDiaryText.split("@@@");
-        this.date = dateFilter(splitRow[0]);
+class ReturnedDiaryEntry {
+    private String entryOne, entryTwo, entryThree, entryFour;
+    private int month, day;
+    ReturnedDiaryEntry(String returnDiaryText){
+        String[] splitRow = returnDiaryText.split("@@@");
+        this.day = Integer.parseInt(splitRow[0].substring(8,10));
+        this.month = Integer.parseInt(splitRow[0].substring(5,7));
         this.entryOne = splitRow[1];
         this.entryTwo = splitRow[2];
         this.entryThree = splitRow[3];
         this.entryFour = splitRow[4];
     }
 
-    public ReturnedDiaryEntry(String date, String entryOne, String entryTwo, String entryThree, String entryFour){
-        this.date = this.dateFilter(date); //this needs to be the day of the month.
-        this.entryOne = entryOne;
-        this.entryTwo = entryTwo;
-        this.entryThree = entryThree;
-        this.entryFour = entryFour;
+    int getMonth() {
+        return month;
     }
 
-    private String dateFilter(String date){
-        return date.substring(6,7);
+    int getDay() {
+        return day;
     }
 
-    public String getDate() {
-        return date;
-    }
-
-    public String getEntryOne() {
+    String getEntryOne() {
         return entryOne;
     }
 
-    public String getEntryTwo() {
+    String getEntryTwo() {
         return entryTwo;
     }
 
-    public String getEntryThree() {
+    String getEntryThree() {
         return entryThree;
     }
 
-    public String getEntryFour() {
+    String getEntryFour() {
         return entryFour;
     }
 }
